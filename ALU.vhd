65e230440d5cf50ad9ac02c80902a78e3494891e
Aya-Mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2021-06-14 19:07:49 +0200

    load immediate shift

--- ALU.vhd
+++ ALU.vhd
@@ -1,5 +1,6 @@
 Library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 ENTITY ALU IS
 GENERIC (n: integer := 8);
@@ -32,12 +33,16 @@ F <= A and B WHEN s="0110"
 ELSE A or B WHEN S="0111"
 Else sigadd WHEN S="0100" or s="0101" or s="0001" or s="0010"
 ELSE not B WHEN S="0000"
-ELSE A WHEN S="0011";
+ELSE A WHEN S="0011"
+ELSE std_logic_vector(shift_left(unsigned(A), to_integer(unsigned(B)))) WHEN S="1000" 
+ELSE std_logic_vector(shift_right(unsigned(A), to_integer(unsigned(B)))) WHEN S="1001"
+ELSE B WHEN S="1011";
 
 sigA<=A WHEN S="0100"
 ELSE not A WHEN S="0101"
 ELSE "00000000000000000000000000000001" WHEN s="0001"
 ELSE "11111111111111111111111111111111" WHEN s="0010";
+
 
 cin<='1' when S="0101"
 ELSE '0';
